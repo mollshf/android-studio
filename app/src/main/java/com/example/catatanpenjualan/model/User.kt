44eab8665d80819eadb949333d74bf44b96ea7f7
mollshf5 <kikymaulana00@gmail.com>
2023-03-06 15:14:28 +0700

    first commit

---
+++ app/src/main/java/com/example/catatanpenjualan/model/User.kt
@@ -0,0 +1,30 @@
+package com.example.catatanpenjualan.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+import javax.annotation.Generated
+
+
+@Generated("com.robohorse.robopojogenerator")
+data class User (
+
+        @field:SerializedName("password")
+        var password:String?=null,
+
+        @field:SerializedName("password")
+        var hp:String?=null,
+
+        @field:SerializedName("password")
+        var id_user:String?=null,
+
+        @field:SerializedName("password")
+        var create_date:String?=null,
+
+        @field:SerializedName("password")
+        var email:String?=null,
+
+        @field:SerializedName("password")
+        var username:String?=null
+
+            ): java.io.Serializable
+
